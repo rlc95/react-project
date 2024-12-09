e203574d240da88c5db459d00ac2027b4e8249c1
Garumuni Rangana Lakshan Chandrasekara <ranganalakch@gmail.com>
2024-12-09 20:45:26 +0530

    creating login button

---
+++ app/login/page.jsx
@@ -0,0 +1,14 @@
+
+export default function loginPage() {
+    return (
+        <div className="flex flex-col justify-center items-center w-full min-h-screen bg-yellow-200 text-black text-center">
+        
+            <div className="bg-green-300 px-5 py-2 rounded-lg">
+                Login 
+            </div>
+        
+        </div>
+    )
+}
+
+rf
