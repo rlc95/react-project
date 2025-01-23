a36b8207f97a1e7475e788b086d6d8ad26f7853f
Garumuni Rangana Lakshan Chandrasekara <ranganalakch@gmail.com>
2025-01-23 20:09:51 +0530

    updated dashboard

--- app/api/v1/movies/route.js
+++ app/api/v1/movies/route.js
@@ -1,18 +1,25 @@
+
+
+import clientPromise from "@/lib/mongodb";
 import { NextResponse } from "next/server"
 
-
-const Movie = [{id:1, title:"load of the ring"},
-    {id:2, title:"scooby doo"},
-    {id:3, title:"zooro"},
-    {id:4, title:"hobbit"},
-    {id:5, title:"harry potter 1"},
-    {id:6, title:"harry potter 2"},
-    {id:7, title:"harry potter 3"},
-    {id:8, title:"harry potter 4"},
-];
-
-
 export const GET = async (req) => {
-    return NextResponse.json({success: true, movies: Movie })
+
+    try {
+        const client = await clientPromise();
+        const db = client.db("sample_mflix"); // db name
+
+        const movies= await db
+            .collection("movies")
+            .find({})
+            .sort({metacritic: -1})
+            .limit(10)
+            .toArray();
+
+        return NextResponse.json(movies);
+    } catch (error) {
+        console.log("DBDataErr",error);
+        return NextResponse.json({error: "Invaild server error"},{status: 500});
+    }
 
 }
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,18 @@
+
+
+import { Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle, } from "@/components/ui/card";
 import { getMovies } from "@/lib/apis/server"
+import Image from "next/image";
 
 export default async function Dashboardpage() {
 
-  const {movies} = await getMovies();
-  console.log("movies",movies);
+  const moviesQuery = await getMovies();
+  console.log("movies",moviesQuery);
   
   return (
     <main>
@@ -18,8 +27,8 @@ export default async function Dashboardpage() {
 
      <div className="container mt-8">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-            
-            {/*
+
+          {/*
             <div className="h-96 bg-yellow-400">Div 2</div>
             <div className="h-96 bg-blue-400">Div 3</div>
             <div className="h-96 bg-orange-400">Div 4</div>
@@ -28,13 +37,29 @@ export default async function Dashboardpage() {
             <div className="h-96 bg-lime-400">Div 7</div>
             <div className="h-96 bg-cyan-400">Div 8</div> */}
 
-              {movies?.length && movies.map((movies) => (
-                <div key={movies?.id} className="h-96 bg-green-400">{movies?.title}</div>
-              ))};
+              {moviesQuery?.length && moviesQuery.map((movies) => (
+                <div key={movies?._id} className="h-96">
+                  
+                  <Card className="h-full">
+                    <CardHeader>
+                      <CardTitle>{movies?.title}</CardTitle>
+                      <CardDescription className="sr-only">{movies?.title}</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="flex justify-center bg-black w-full h-[276px] mb-4 rounded">
+                      <Image src={movies?.poster} alt={movies?.title} width={200} height={400} className="h-full w-auto object-contain" />
+                      </div>
+                    </CardContent>
+                    <CardFooter>
+                      
+                    </CardFooter>
+                  </Card>
 
+                </div>
+              ))};     
+            
         </div>
      </div>
-
 
     </main>
     
